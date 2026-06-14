9a1ae361f5ccbb4aaade6a07026bf80ebdf71562
Владислав Дружицький <druzhytskyivlad@gmail.com>
2026-06-14 23:08:59 +0300

    modified:   mysite/db.sqlite3 	new file:   mysite/db2.sqlite3 	new file:   mysite/db3.sqlite3 	modified:   mysite/mysite/__pycache__/settings.cpython-314.pyc 	modified:   mysite/mysite/settings.py 	modified:   mysite/polls/__pycache__/views.cpython-314.pyc

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -73,13 +73,43 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db3.sqlite3',
+#     }
+# }
+
+
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db2.sqlite3',
+#     }
+# }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "OPTIONS": {
+#             "service": "my_service",
+#             "passfile": ".my_pgpass"
+#         }
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',      
+        'USER': 'postgres',           
+        'PASSWORD': 'POSTGRES',     
+        'HOST': '127.0.0.1',          
+        'PORT': '5432',               
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
